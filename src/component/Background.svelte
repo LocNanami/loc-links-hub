8494cd05ea2d61ff592da5bbfce85776990144da
Loc Nanami <nanami.loc@gmail.com>
2025-02-13 17:28:15 +0000

    feat: 幾何学的背景コンポーネントを新規作成し、ページに追加

---
+++ src/component/Background.svelte
@@ -0,0 +1,75 @@
+<div class="root">
+	<div class="geometric-bg"></div>
+</div>
+
+<style>
+	.root {
+		background: linear-gradient(-45deg, #1e3c72, #2a5298, #2980b9, #2c3e50);
+		animation: gradientBG 15s ease infinite;
+		background-size: 400% 400%;
+		overflow: hidden;
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+	@keyframes gradientBG {
+		0% {
+			background-position: 0% 50%;
+		}
+		50% {
+			background-position: 100% 50%;
+		}
+		100% {
+			background-position: 0% 50%;
+		}
+	}
+	.geometric-bg {
+		z-index: -1;
+		opacity: 0.3;
+	}
+	.geometric-bg::before,
+	.geometric-bg::after {
+		content: '';
+		position: absolute;
+		width: 200vmax;
+		height: 200vmax;
+		top: -100vmax;
+		left: 50%;
+		transform: translate(-50%, 0);
+		background: rgba(255, 255, 255, 0.1);
+		animation: rotate 30s linear infinite;
+	}
+	.geometric-bg::before {
+		border-radius: 37% 29% 27% 27% / 28% 25% 41% 37%;
+	}
+	.geometric-bg::after {
+		border-radius: 26% 33% 33% 35% / 25% 15% 41% 39%;
+		animation-duration: 40s;
+	}
+
+	@keyframes rotate {
+		0% {
+			transform: translate(-50%, 0) rotate(0deg);
+		}
+		100% {
+			transform: translate(-50%, 0) rotate(360deg);
+		}
+	}
+
+	.square {
+		position: absolute;
+		background: rgba(255, 255, 255, 0.5);
+		animation: move-squares linear infinite;
+	}
+
+	@keyframes move-squares {
+		0% {
+			transform: translateX(-100vw);
+		}
+		100% {
+			transform: translateX(100vw);
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import { type IconDefinition } from '@fortawesome/free-regular-svg-icons';
 	import { faYoutube, faXTwitter, faBluesky } from '@fortawesome/free-brands-svg-icons';
 	import { faMicrophone } from '@fortawesome/free-solid-svg-icons';
+	import Background from '../component/Background.svelte';
 
 	interface SocialLink {
 		name: string;
@@ -30,7 +31,7 @@
 	/>
 </svelte:head>
 
-<div class="geometric-bg"></div>
+<Background />
 <div class="main">
 	<div class="container head">
 		<div class="image">
@@ -62,54 +63,7 @@
 		padding: 32px;
 		min-height: 100vh;
 		overflow: hidden;
-		background: linear-gradient(-45deg, #1e3c72, #2a5298, #2980b9, #2c3e50);
-		background-size: 400% 400%;
-		animation: gradientBG 15s ease infinite;
 		min-width: 250px;
-	}
-
-	@keyframes gradientBG {
-		0% {
-			background-position: 0% 50%;
-		}
-		50% {
-			background-position: 100% 50%;
-		}
-		100% {
-			background-position: 0% 50%;
-		}
-	}
-
-	.geometric-bg {
-		position: fixed;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-		z-index: -1;
-		opacity: 0.3;
-	}
-
-	.geometric-bg::before,
-	.geometric-bg::after {
-		content: '';
-		position: absolute;
-		width: 200vmax;
-		height: 200vmax;
-		top: -100vmax;
-		left: 50%;
-		transform: translate(-50%, 0);
-		background: rgba(255, 255, 255, 0.1);
-		animation: rotate 30s linear infinite;
-	}
-
-	.geometric-bg::before {
-		border-radius: 37% 29% 27% 27% / 28% 25% 41% 37%;
-	}
-
-	.geometric-bg::after {
-		border-radius: 26% 33% 33% 35% / 25% 15% 41% 39%;
-		animation-duration: 40s;
 	}
 
 	@keyframes rotate {
